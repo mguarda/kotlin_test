bfb95105d991d008e9a2c7d8520492cee4c1bc7a
Mauricio Guarda <mauricio.guarda@west-ingenieria.cl>
2017-07-01 23:25:35 -0400

    Initial commit
    Initializing Project

---
+++ app/src/main/java/com/example/mauri/kotlin_test/test_kotlin.kt
@@ -0,0 +1,12 @@
+package com.example.mauri.kotlin_test
+
+import android.app.Activity
+import android.os.Bundle
+
+class test_kotlin : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_kotlin)
+    }
+}
